00cd8bd193b1c9a98b5d758a7022e71c761e29a5
Manulaiko <manulaiko@gmail.com>
2016-12-08 18:49:09 +0100

    Versioning

--- src/com/manulaiko/filenator/launcher/Main.java
+++ src/com/manulaiko/filenator/launcher/Main.java
@@ -22,7 +22,7 @@ public class Main
     /**
      * Application's version.
      */
-    public static final String version = "0.0.0";
+    public static final String version = "1.0.0";
 
     /**
      * Main method.
